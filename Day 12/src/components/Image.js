4d8896a10c0e9425021f0c519f00de2e7c1c2604
Sameer ali <93761571+sameer0288@users.noreply.github.com>
2022-07-20 08:49:41 +0530

    Update Image.js

--- Day 12/src/components/Image.js
+++ Day 12/src/components/Image.js
@@ -1,14 +1,10 @@
-const Image = ({ data }) => {
-    return ( 
-        < a href = { data.urls.regular }
-        target = "_blank"
-        rel = "noreferrer" >
-        <
-        img className = "h-72 w-full object-cover rounded-lg shadow-md"
-        src = { data.urls.small }
-        />{" "} <
-        /a>
-    );
-};
 
-export default Image;
+const Image = ({ data }) => {
+  return (
+    <a href={data.urls.regular} target="_blank" rel="noreferrer">
+      <img className="h-72 w-full object-cover rounded-lg shadow-md" src={data.urls.small} alt={data.alt_description} />
+    </a>
+  )
+}
+
+export default Image
